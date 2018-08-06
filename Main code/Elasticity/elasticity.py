8b0ad4c171e77fa762abd8be221620a73809bcab
MostafaMollaali <m.molaali@gmail.com>
2018-08-06 20:15:31 +0800

    Add files via upload

--- Main code/Elasticity/elasticity.py
+++ Main code/Elasticity/elasticity.py
@@ -22,6 +22,8 @@ import sympy, sys, math, os, subprocess, shutil
 import petsc4py
 petsc4py.init()
 from petsc4py import PETSc
+from numpy import loadtxt 
+from scipy.interpolate import interp1d
 
 
 mpi_process_rank = dolfin.cpp.common.MPI_rank(mpi_comm_world())
@@ -56,13 +58,13 @@ solver_u_parameters ={"linear_solver", "mumps", # prefer "superlu_dist" or "mump
 # Input date
 #=======================================================================================
 # Geometry
-L = 1.0 # length
-H = 0.4 # height
-hsize= 0.01 # target cell size
+L = 170 # length
+H = 170 # height
+
 
 # Material constants
-E = 10. # Young modulus
-nu = 0.3 # Poisson ratio
+E = 6.e3 # Young modulus
+nu = 0.34 # Poisson ratio
 
 PlaneStress= False
 
@@ -76,7 +78,7 @@ body_force = Constant((0.,0.))  # bulk load
 #=======================================================================================
 # Geometry and mesh generation
 #=======================================================================================
-mesh = RectangleMesh(Point(0., 0.), Point(1., 0.4), 100, 40)
+mesh = RectangleMesh(Point(0., 0.), Point(170, 170), 100, 100)
 plt.figure(1)
 plot(mesh, "2D mesh")
 plt.interactive(True)
@@ -106,9 +108,14 @@ def psi_0(u_):
 #=======================================================================================
 # others definitions
 #=======================================================================================
-ell=0.1
-law='elasticity'
-prefix = "L%s-H%.2f-S%.4f"%(L,H,hsize)
+dispMehtod = False
+
+if not dispMehtod:  
+	Mehtod= 'StressBC';
+else: 
+	Mehtod = 'DispBC'
+
+prefix = "L%s-H%.2f-%s"%(L,H,Mehtod)
 save_dir = "Elasticity_result/" + prefix + "/"
 
 if os.path.isdir(save_dir):
@@ -117,6 +124,7 @@ if os.path.isdir(save_dir):
 # zero and unit vectors
 zero_v = Constant((0.,)*ndim)
 e1 = [Constant([1.,0.]),Constant((1.,0.,0.))][ndim-2]
+e2 = [Constant([0.,1.]),Constant((0.,1.,0.))][ndim-2]
 
 # plane strain or plane stress
 if not PlaneStress:  # plane strain
@@ -126,6 +134,8 @@ else:  # plane stress
 
 # shear modulus
 mu = E / (2.0 * (1.0 + nu)) 
+
+
 #=======================================================================================
 # Variational formulation
 #=======================================================================================
@@ -138,24 +148,77 @@ u_, u, u_t = Function(V_u), TrialFunction(V_u), TestFunction(V_u)
 # Dirichlet boundary condition for a traction test boundary
 #=======================================================================================
 # bc - u (imposed displacement)
-def left_boundary(x, on_boundary):
-    tol = 1E-14 # tolerance for coordinate comparisons
-    return on_boundary and abs(x[0]) < tol
+class Right(SubDomain):
+    def inside(self, x, on_boundary):
+        return near((x[0] - L) * 0.01, 0)
+        
+class Left(SubDomain):
+    def inside(self, x, on_boundary):
+        return near((x[0]) * 0.01, 0.)
 
-def right_boundary(x, on_boundary):
-    tol = 1E-14 # tolerance for coordinate comparisons
-    return on_boundary and abs(x[0] -L ) < tol
+class Top(SubDomain):
+    def inside(self, x, on_boundary):
+        return near((x[1] - H) * 0.01, 0)
+        
+class Bottom(SubDomain):
+    def inside(self, x, on_boundary):
+        return near((x[1]) * 0.01, 0.)
+
+class vary(SubDomain):
+    def inside(self, x, on_boundary):
+        return near((x[1]) * 0.01, 0.)
+
     
 e1 =  [Constant([1.,0.]),Constant((1.,0.,0.))][ndim-2]
-bc_left_u = DirichletBC(V_u, zero_v, left_boundary)
-bc_right_u = DirichletBC(V_u.sub(0), .1, right_boundary)
-bc_u = [bc_left_u, bc_right_u]
+# Initialize sub-domain instances
+right=Right()
+left=Left()
+bottom=Bottom()
+top= Top()
 
+# define meshfunction to identify boundaries by numbers
+boundaries = FacetFunction("size_t", mesh)
+boundaries.set_all(9999)
+right.mark(boundaries, 1) # mark top as 1
+left.mark(boundaries, 2) # mark top as 2
+bottom.mark(boundaries, 3) # mark top as 2
+top.mark(boundaries, 4) # mark top as 2
+
+
+##############################################################################################################
+sigma_R =  Constant([-6.,0.]) #traction on right boundary
+sigma_L =  Constant([6.,0.]) #traction on right boundary
+sigma_B =  Constant([0., 6.]) #traction on right boundary
+sigma_T =  Constant([0.,-6.]) #traction on right boundary
+
+##############################################################################################################
+u_R = Constant([-0.0264, 0.0264]) #zero_v #Expression(("p", "0.",), p=0.0, degree=1)
+u_L = Constant([0.0264, -0.0264])#zero_v
+u_T = Constant([0.0264, -0.0264])#zero_v#Expression(("0.", "p",), p=0.0, degree=1)
+u_B = Constant([-0.0264, 0.0264])#zero_v#Expression(("0.", "-p",), p=0.0, degree=1)
+
+Gamma_u_0 = DirichletBC(V_u, u_R, boundaries, 1)
+Gamma_u_1 = DirichletBC(V_u, u_L, boundaries, 2)
+Gamma_u_2 = DirichletBC(V_u, u_B, boundaries, 3)
+Gamma_u_3 = DirichletBC(V_u, u_T, boundaries, 4)
+
+if not dispMehtod:  
+	bc_u=[];
+else:  
+	bc_u = [Gamma_u_0, Gamma_u_1, Gamma_u_2, Gamma_u_3]
+
+# Define new measure including boundary naming 
+ds = Measure("ds")[boundaries] # left: ds(1), right: ds(2)
 #====================================================================================
 # Define  problem and solvers
 #====================================================================================
 elastic_energy = psi_0(u_)*dx
-external_work = dot(body_force, u_)*dx
+
+if not dispMehtod:  
+	external_work = dot(body_force, u_)*dx+ dot(sigma_R, u_)*ds(1)+ dot(sigma_L, u_)*ds(2)+ dot(sigma_T, u_)*ds(4)+dot(sigma_B, u_)*ds(3);
+else: 
+	external_work = dot(body_force, u_)*dx
+
 total_energy = elastic_energy - external_work
 
 # First derivatives of energies (Residual)
@@ -183,10 +246,32 @@ if mpi_process_rank == 0:
     print "The elastic energy is %g" % energy_value
     print "-----------------------------------------"
 
+u_nodal_values = u_.vector()
+u_array = u_nodal_values.array()
+coor = mesh.coordinates()
+center = (85,85)
+print u_(center)
+############################################################################################
+
 # Dump solution to file in VTK format
 File(save_dir+'/u.pvd') << u_
 
 # Plot solution
 plot(u_,mode = "displacement", title = 'Displacement')
 plot(sqrt(inner(sigma0(u_),sigma0(u_))), title = 'stress norm')
+
+# Project and write stress field to post-processing file
+W = TensorFunctionSpace(mesh, "Discontinuous Lagrange", 0)
+V = FunctionSpace(mesh, 'P', 1)
+
+stress = project(sigma0(u_), V=W)
+File(save_dir+"stress.pvd") << stress
+
+s = sigma0(u_) - (1./3)*tr(sigma0(u_))*Identity(ndim)  # deviatoric stress
+von_Mises = sqrt(3./2*inner(s, s))
+V = FunctionSpace(mesh, 'P', 1)
+von_Mises = project(von_Mises, V)
+File(save_dir+"von_Mises.pvd") << von_Mises
+
+
 
